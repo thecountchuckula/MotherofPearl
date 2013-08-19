753d20b71f34bb2c8d978a94c13fddefb552039b
Benjamin K <bk1325@gmail.com>
2013-08-18 23:08:35 -0700

    I fixed it its all pretty and stuff
    Removed a dupe bug and reorganised EVERYTHING.

--- common/tcc/MotherOfPearl/ModMain.java
+++ common/tcc/MotherOfPearl/ModMain.java
@@ -4,7 +4,11 @@ import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.Configuration;
+import tcc.MotherOfPearl.armor.ModArmor;
+import tcc.MotherOfPearl.blocks.ModBlocks;
+import tcc.MotherOfPearl.items.ModItems;
 import tcc.MotherOfPearl.proxy.CommonProxy;
+import tcc.MotherOfPearl.tools.ModTools;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
@@ -12,117 +16,66 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import darkevilmac.MotherOfPearl.Config.MOPConfiguration;
+import darkevilmac.MotherOfPearl.lib.Reference;
+import darkevilmac.MotherOfPearl.misc.Crafting;
+import darkevilmac.MotherOfPearl.misc.LanguageReg;
 
-@Mod(modid = ModInfo.MOD_ID, name = ModInfo.NAME, version = ModInfo.version)
-@NetworkMod(channels = { ModInfo.MOD_ID }, clientSideRequired = true, serverSideRequired = true)
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.version)
+@NetworkMod(channels = { Reference.MOD_ID }, clientSideRequired = true, serverSideRequired = true)
 public class ModMain {
 
     public static CreativeTabs tabMoP = new CreativeTabs("MoP") {
         @Override
         public ItemStack getIconItemStack() {
-            return new ItemStack(ModInfo.pearl, 1, 0);
+            return new ItemStack(ModItems.pearl, 1, 0);
         }
     };
     public static CreativeTabs tabMoP_WIP = new CreativeTabs("MoP_WIP") {
         @Override
         public ItemStack getIconItemStack() {
-            return new ItemStack(ModInfo.braShell, 1, 0);
+            return new ItemStack(ModArmor.braShell, 1, 0);
         }
     };
 
-    static int blackPearlID;
-    static int bowPearlID;
-    static int knifePearlID;
-    static int oysterID;
-    static int oysterMeatID;
-    static int pearlID;
-    static int shellID;
-
-    static int shovelPearlID;
-    static int swordPearlID;
-    static int bootsPearlID;
-    static int braShellID;
-    static int chestplatePearlID;
-    static int helmetPearlID;
-    static int leggingsPearlID;
-    static int pearlstoneID;
-    static int blackPearlstoneID;
-    static int pearlstoneBrickID;
-    static int blackPearlstoneBrickID;
-    static int clayOysterID;
-    static int bootsBlackPearlID;
-    static int chestplateBlackPearlID;
-    static int helmetBlackPearlID;
-    static int leggingsBlackPearlID;
-    static int skirtGrassID;
-    static int shovelBlackPearlID;
-    static int swordBlackPearlID;
-    static int crownShellID;
-    static int hempSandalsID;
-
-    @SidedProxy(clientSide = ModInfo.PROXY_PATH + ".ClientProxy", serverSide = ModInfo.PROXY_PATH + ".CommonProxy")
+    @SidedProxy(clientSide = Reference.PROXY_PATH + ".ClientProxy", serverSide = Reference.PROXY_PATH + ".CommonProxy")
     public static CommonProxy proxy;
+    public static Configuration cfg;
+    private static Configuration config;
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent event) {
-        Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-        config.load();
-        // Items
-        blackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearl", 5100).getInt();
-        bootsPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Boots", 5101).getInt();
-        bowPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Bow", 5102).getInt();
-        braShellID = config.get(Configuration.CATEGORY_ITEM, "Shell Bra", 5103).getInt();
-        chestplatePearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Chestplate", 5104).getInt();
-        helmetPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Helmet", 5105).getInt();
-        knifePearlID = config.get(Configuration.CATEGORY_ITEM, "Pearling Knife", 5106).getInt();
-        leggingsPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Leggings", 5107).getInt();
-        oysterID = config.get(Configuration.CATEGORY_ITEM, "Oyster", 5108).getInt();
-        oysterMeatID = config.get(Configuration.CATEGORY_ITEM, "Oyster Meat", 5109).getInt();
-        pearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl", 5110).getInt();
-        shellID = config.get(Configuration.CATEGORY_ITEM, "Shell", 5111).getInt();
-        shovelPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Shovel", 5112).getInt();
-        swordPearlID = config.get(Configuration.CATEGORY_ITEM, "Pearl Sword", 5113).getInt();
-        bootsBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearly Boots", 5114).getInt();
-        chestplateBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearly Chestplate", 5115).getInt();
-        helmetBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearly Helmet", 5116).getInt();
-        leggingsBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearly Leggings", 5117).getInt();
-        skirtGrassID = config.get(Configuration.CATEGORY_ITEM, "Grass Skirt", 5118).getInt();
-        shovelBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearl Shovel", 5119).getInt();
-        swordBlackPearlID = config.get(Configuration.CATEGORY_ITEM, "Black Pearl Sword", 5120).getInt();
-        crownShellID = config.get(Configuration.CATEGORY_ITEM, "Crown of Shells", 5121).getInt();
-        hempSandalsID = config.get(Configuration.CATEGORY_ITEM, "Hemp Sandals", 5122).getInt();
-        // Blocks
-        pearlstoneID = config.get(Configuration.CATEGORY_BLOCK, "Pearly Stone", 510).getInt();
-        blackPearlstoneID = config.get(Configuration.CATEGORY_BLOCK, "Black Pearly Stone", 511).getInt();
-        pearlstoneBrickID = config.get(Configuration.CATEGORY_BLOCK, "Pearly Stone Brick", 512).getInt();
-        blackPearlstoneBrickID = config.get(Configuration.CATEGORY_BLOCK, "Black Pearly Stone Brick", 513).getInt();
-        clayOysterID = config.get(Configuration.CATEGORY_BLOCK, "Oyster Yielding Clay", 514).getInt();
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        cfg = config;
 
-        config.save();
+        /*
+         * I have begun reorganizing some code no idea what i've broken but I'm
+         * working on making it cleaner.
+         */
 
-        ModInfo.initItem();
-        ModInfo.initTools();
-        ModInfo.initArmor();
-        ModInfo.initBlock();
-        ModInfo.languageRegistry();
-        ModInfo.crafting();
+        MOPConfiguration.init();
+        ModBlocks.init();
+        ModItems.init();
+        ModTools.init();
+        ModArmor.init();
+        LanguageReg.init();
+
         /*
          * darkevilmac pointed me to the website
          * http://www.minecraftforum.net/topic
          * /1578638-forge-how-to-edit-vanilla-block-properties/ to help me
          * replace clayGen in the world.
          */
-        Block.blocksList[Block.blockClay.blockID] = ModInfo.clayOyster;
+        Block.blocksList[Block.blockClay.blockID] = ModBlocks.clayOyster;
     }
 
     @EventHandler
     public static void init(FMLInitializationEvent event) {
-
+        Crafting.init();
     }
 
     @EventHandler
     public static void postInit(FMLPostInitializationEvent event) {
-
     }
 
 }
---
+++ common/tcc/MotherOfPearl/tools/ModTools.java
@@ -0,0 +1,76 @@
+package tcc.MotherOfPearl.tools;
+
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.EnumHelper;
+import tcc.MotherOfPearl.items.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import darkevilmac.MotherOfPearl.Config.MOPConfiguration;
+import darkevilmac.MotherOfPearl.lib.ItemStacks;
+
+public class ModTools {
+
+    public static Item shovelPearl;
+    public static Item shovelBlackPearl;
+    public static Item swordPearl;
+    public static Item swordBlackPearl;
+    public static Item bowPearl;
+    public static Item knifePearl;
+
+    public static EnumToolMaterial pearlShovel = EnumHelper.addToolMaterial("PearlShovel", 2, 256, 2f, 3f, 25);
+    public static EnumToolMaterial pearlSword = EnumHelper.addToolMaterial("PearlSword", 2, 256, 1f, 2f, 25);
+    public static EnumToolMaterial pearlKnife = EnumHelper.addToolMaterial("PearlKnife", 2, 1000, .5f, 1f, 25);
+
+    public static void init() {
+        defTools();
+        initGameRegistry();
+        initLanguageRegistry();
+    }
+
+    public static void defTools() {
+        bowPearl = new ItemPearlBow(MOPConfiguration.bowPearlID);
+        knifePearl = new ItemPearlKnife(MOPConfiguration.knifePearlID);
+        shovelPearl = new ItemPearlShovel(MOPConfiguration.shovelPearlID);
+        shovelBlackPearl = new ItemBlackPearlShovel(MOPConfiguration.shovelBlackPearlID);
+        swordPearl = new ItemPearlSword(MOPConfiguration.swordPearlID);
+        swordBlackPearl = new ItemBlackPearlSword(MOPConfiguration.swordBlackPearlID);
+    }
+
+    public static void initGameRegistry() {
+
+    }
+
+    public static void initCrafting() {
+        GameRegistry.addRecipe(new ItemStack(shovelPearl, 1), "y", "x", "x", 'x', ItemStacks.stick, 'y', ModItems.pearl);
+        GameRegistry.addRecipe(new ItemStack(shovelBlackPearl, 1), "y", "x", "x", 'x', ItemStacks.stick, 'y', ModItems.blackPearl);
+        GameRegistry.addRecipe(new ItemStack(swordPearl, 1), "y", "y", "x", 'x', ItemStacks.stick, 'y', ModItems.pearl);
+        GameRegistry.addRecipe(new ItemStack(swordBlackPearl, 1), "y", "y", "x", 'x', ItemStacks.stick, 'y', ModItems.blackPearl);
+        // GameRegistry.addRecipe(new ItemStack(bowPearl, 1),
+        // "zx ",
+        // "z y",
+        // "zx ",
+        // 'x', stick,
+        // 'y', pearl,
+        // 'z', string);
+        // GameRegistry.addRecipe(new ItemStack(bowPearl, 1),
+        // " xz",
+        // "y z",
+        // " xz",
+        // 'x', stick,
+        // 'y', pearl,
+        // 'z', string);
+        GameRegistry.addRecipe(new ItemStack(knifePearl, 1), "y", "x", 'x', ItemStacks.stick, 'y', ModItems.pearl);
+    }
+
+    public static void initLanguageRegistry() {
+        LanguageRegistry.addName(bowPearl, "Pearl Bow");
+        LanguageRegistry.addName(knifePearl, "Pearling Knife");
+        LanguageRegistry.addName(shovelPearl, "Pearl Shovel");
+        LanguageRegistry.addName(shovelBlackPearl, "Black Pearl Shovel");
+        LanguageRegistry.addName(swordPearl, "Pearl Sword");
+        LanguageRegistry.addName(swordBlackPearl, "Black Pearl Sword");
+    }
+
+}
